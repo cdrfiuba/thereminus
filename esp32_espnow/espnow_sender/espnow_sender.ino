bb76737671a6c8e7126141c1e2e44128abf964a6
Misael.K <MisaelK@users.noreply.github.com>
2024-09-27 21:42:57 -0300

    fix(espnow_sender): add debug in espnow callback

--- esp32_espnow/espnow_sender/espnow_sender.ino
+++ esp32_espnow/espnow_sender/espnow_sender.ino
@@ -134,8 +134,9 @@ void loop() {
 
 // callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "OK" : "FAIL");
-  Serial.print(micros() - timestamp);
-  Serial.println("ms");
+  if (DEBUG) {
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "OK" : "FAIL");
+    Serial.print(micros() - timestamp);
+    Serial.println("ms");
+  }
 }
- 
